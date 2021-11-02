baf05572fe32167660a33668912a2aa485247cff
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 22:08:56 -0400

    Create ArticleItem component with boilerplate code. Import Next Link and article CSS modules

---
+++ components/ArticleItem.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import articleStyles from '../styles/Article.module.css';
+
+const ArticleItem = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default ArticleItem;
