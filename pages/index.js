5e168c36b38758bef7fee950577513e0068784fc
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 21:55:17 -0400

    Add forward slashes to API route

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https:jsonplaceholder.typicode.com/posts?_limit=6`);
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
   const articles = await res.json();
 
   return {
