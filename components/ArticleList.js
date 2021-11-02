0e4991d671fab45b68ac96d1ed8b441450225d35
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 22:05:10 -0400

    Build ArticleList component

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,7 +1,13 @@
 import articleStyles from '../styles/Article.module.css';
 
-const ArticleList = () => {
-
+const ArticleList = ({articles}) => {
+    return (
+        <div className={articleStyles.grid}>
+            {articles.map(article => (
+                <h3>{article.title}</h3>
+            ))}
+        </div>
+    )
 }
 
 export default ArticleList;
