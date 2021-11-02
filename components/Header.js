f8b99fcfd3732dbdd92928d08825cdae4d606b9f
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 21:40:36 -0400

    Build Header component

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,10 @@ import headerStyles from '../styles/Header.module.css';
 
 const Header = () => {
     return (
-        <div></div>
+        <div>
+            <h1 className={headerStyles.title}><span>Bring Dat</span> Heat 🔥</h1>
+            <p className={headerStyles.description}>Keep up to date with the latest Next.js crash courses.</p>
+        </div>
     )
 }
 
