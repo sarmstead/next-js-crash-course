f38c34199548afc894fbcad6b4d2ef861b4124ae
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 22:18:37 -0400

    Fix html code for right arrow after article title

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -5,7 +5,7 @@ const ArticleItem = ({article}) => {
     return (
         <Link href='/article/[id]' as={`/article/${article.id}`}>
             <a className={articleStyles.card}>
-                <h3>{article.title} $rarr;</h3>
+                <h3>{article.title} &rarr;</h3>
                 <p>{article.body}</p>
             </a>
         </Link>
