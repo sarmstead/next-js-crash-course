861236402a54dcb37de5c958984350e67f395469
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 22:43:09 -0400

    Transform getServerSideProps into getStaticProps. Add getStaticPaths function.

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -13,7 +13,7 @@ const article = ({article}) => {
 
 }
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
 
     const article = await res.json();
@@ -22,6 +22,17 @@ export const getServerSideProps = async (context) => {
         props: {
             article
         }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+    const article = await res.json();
+    const ids = article.map(article => article.id);
+    const paths = ids.map(id => ({params: {id: id.toString()}}));
+    return {
+        paths,
+        fallback: false
     }
 }
 
