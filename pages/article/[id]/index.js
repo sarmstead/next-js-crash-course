7def84e6ad93268ed0a89d820bcdc3cb198f438b
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 22:30:48 -0400

    Create getServerSideProps function

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,21 @@
+import {useRouter} from 'next/router';
+
 const article = () => {
 
+    return <div></div>
+
+}
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode,com/posts/${context.params.id}`);
+
+    const article = await res.json();
+
+    return {
+        props: {
+            article
+        }
+    }
 }
 
 export default article;
