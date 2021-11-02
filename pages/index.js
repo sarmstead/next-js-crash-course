2ef0ce91cef7cd56e7e6c1302ecde718127fc947
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 21:01:40 -0400

    Add metadata title for home page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Next.js Crash Course</title>
+      </Head>
       <h1>Welcome to Next.js</h1>
     </div>
   )
