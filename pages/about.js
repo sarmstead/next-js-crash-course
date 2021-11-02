0a3f1dcf1a65ab81607fd524bb477b8ee7b63fd5
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-11-01 21:02:13 -0400

    Add metadata title for about page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
 const About = () => {
     return (
         <div>
+            <Head>
+                <title>Next.js Crash Course</title>
+            </Head>
             <h1>About</h1>
         </div>
     )
